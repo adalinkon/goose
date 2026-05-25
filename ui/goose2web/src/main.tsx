42ac73e2d35a723b4b067f2d6a61dac89fce8a4d
adalinkon <dh1608958498@gmail.com>
2026-05-26 00:06:32 +0800

    chore: sync goose2web hash router
    Signed-off-by: adalinkon <dh1608958498@gmail.com>

--- ui/goose2web/src/main.tsx
+++ ui/goose2web/src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
 import { App } from "@/app/App";
 import { I18nProvider } from "@/shared/i18n";
@@ -24,7 +25,9 @@ ReactDOM.createRoot(root).render(
     <QueryClientProvider client={queryClient}>
       <I18nProvider>
         <ThemeProvider defaultTheme="system">
-          <App />
+          <HashRouter>
+            <App />
+          </HashRouter>
         </ThemeProvider>
       </I18nProvider>
     </QueryClientProvider>
